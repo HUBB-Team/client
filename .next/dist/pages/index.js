356ea9ca9c669e641528cc82111fed96b4b03b1e
Grigorii Horos <horosgrisa@gmail.com>
2017-01-29 13:21:47 +0200

    Hello world

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("/home/grisa/\u041F\u0440\u043E\u0435\u043A\u0442\u044B/HUBB-Team/test-client/node_modules/react/react.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+    return _react2.default.createElement(
+        "div",
+        null,
+        " Hello world "
+    );
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OztrQkFBZTtBQUFBLFdBQ1g7QUFBQTtBQUFBO0FBQUE7QUFBQSxLQURXO0FBQUEsQyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZVJvb3QiOiIvaG9tZS9ncmlzYS/Qn9GA0L7QtdC60YLRiy9IVUJCLVRlYW0vdGVzdC1jbGllbnQiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gICAgPGRpdj4gSGVsbG8gd29ybGQgPC9kaXY+XG4pXG4iXX0=

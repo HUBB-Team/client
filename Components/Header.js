862381ec6eef195b96aa0977861f38bd6e15fb59
Grigorii Horos <horosgrisa@gmail.com>
2017-01-31 23:32:04 +0200

    Eslint

---
+++ Components/Header.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <Head>
+        <title> Our </title>
+      </Head>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
-
 export default () => (
-    <div>
-	  <Head>
-	    <title>Main</title>
-	    <link rel="stylesheet" href="/static/main.css" />
-	  </Head>
-	  <h1>Hello world</h1>
-	  <Link href="/about">About</Link>
-	</div>
+  <div>
+    <Head>
+      <title>Main</title>
+      <link rel="stylesheet" href="/static/main.css" />
+    </Head>
+    <h1>Hello world</h1>
+    <Link href="/about">About</Link>
+  </div>
 
 )
-
-
 

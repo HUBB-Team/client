b18b4b5a986a6b1ccb64b7bca529c0a892472e70
Grigorii Horos <horosgrisa@gmail.com>
2017-02-04 12:03:31 +0200

    Initial device

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,9 @@ export default class MyDocument extends Document {
     return (
       <html lang="ro">
         <Head >
-          <title> Our Site </title>
+          <title> Online Map </title>
           <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="stylesheet" href="/static/bower_components/materialize/dist/css/materialize.css" />
-          <link rel="stylesheet" href="static/bower_components/mdi/css/materialdesignicons.min.css" />
-          <link rel="stylesheet" href="/static/css/style.css" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,10 @@
 import Link from 'next/link'
-import Sidebar from '../Components/Sidebar'
 
 export default class Main extends React.Component {
   render () {
     return (
-      <div>
-        <header>
-          <Sidebar />
-        </header>
-        <main>
-          <nav className="top-nav">
-            <div className="container">
-              <div className="nav-wrapper1">
-                <a className="page-title1">HUBB Team</a>
-              </div>
-            </div>
-          </nav>
-          <div className="container">
-            <div className="row">
-              <div className="col s12 m4">
-                <div className="icon-block">
-                  <h2 className="center purple-text">
-                    <i className="large material-icons">code</i>
-                  </h2>
-                  <h5 className="center">Back-End Development</h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                           </p>
-                </div>
-              </div>
-
-              <div className="col s12 m4">
-                <div className="icon-block">
-                  <h2 className="center purple-text">
-                    <i className="large material-icons">dashboard</i>
-                  </h2>
-                  <h5 className="center">Front-End Development</h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                           </p>
-                </div>
-              </div>
-              <div className="col s12 m4">
-                <div className="icon-block">
-                  <h2 className="center purple-text">
-                    <i className="large material-icons">group</i>
-                  </h2>
-                  <h5 className="center">User Experience Focused</h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                           </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-        <footer >
-          <div className="footer-copyright">
-            <div className="container center">
-                Made by <Link className="purple" href="/team"><span>HUBB Team</span></Link>
-            </div>
-          </div>
-        </footer>
-        <style jsx>{`
-        footer {
-          position: 'fixed';
-          right: 0;
-          bottom: 0;
-          left: 0;
-          'padding-top': '1rem';
-          'padding-bottom': '1rem';
-          'background-color': '#efefef';
-          'text-align': 'center';
-        }
-        .container .nav-wrapper1 {
-          text-align: center;
-          padding-top: 80px;
-        }
-      `}</style>
-      </div>
+      <Link href="/">Main</Link>
     )
   }
+
 }

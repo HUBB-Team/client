e7e40846e9fc5adc6a7d7133da9f111f38543da3
Grigorii Horos <horosgrisa@gmail.com>
2017-02-04 13:06:33 +0200

    Initial

---
+++ functions/frontend.js
@@ -0,0 +1,4 @@
+export const selPerson = (id) => {
+  console.log(id)
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import { Dropdown, NavItem, Navbar } from 'react-materialize'
 
-export default class Main extends React.Component {
+import {selPerson} from '../functions/frontend'
+
+export default class Default extends React.Component {
   render () {
     return (
-      <Link href="/">Main</Link>
+      <div>
+        <Navbar brand="Online Map" right>
+          <Dropdown trigger={
+            <a className="dropdown-button" href="#!" data-activates="dropdown1">Lucratori<i className="material-icons right">arrow_drop_down</i></a>
+          }>
+            <NavItem onClick={() => { selPerson(1) }}>Colea</NavItem>
+            <NavItem onClick={() => { selPerson(2) }}>Fedea</NavItem>
+            <NavItem onClick={() => { selPerson(3) }}>Vanea</NavItem>
+          </Dropdown>
+        </Navbar>
+        <main >
+          <div ref="map" className="map" >
+            map
+          </div>
+        </main>
+        <style jsx>{`
+          main{
+            width:100%;
+            overflow:auto;
+          }
+          
+          .map{
+            background-color: #e43;
+            width: 100%;
+            min-width: 1000px;
+            min-height:500px;
+          }
+        `}</style>
+      </div>
     )
   }
+
 }
